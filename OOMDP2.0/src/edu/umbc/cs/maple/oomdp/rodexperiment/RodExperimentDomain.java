50ddf774df71642cc4311643ca977c999e306ed6
Bhuvana Bellala <bbella1@umbc.edu>
2013-07-03 10:38:41 -0400

    Adding a new rodExperiment domain

---
+++ OOMDP2.0/src/edu/umbc/cs/maple/oomdp/rodexperiment/RodExperimentDomain.java
@@ -0,0 +1,18 @@
+package edu.umbc.cs.maple.oomdp.rodexperiment;
+
+
+
+import edu.umbc.cs.maple.domain.oomdp.DomainGenerator;
+import edu.umbc.cs.maple.oomdp.Domain;
+
+public class RodExperimentDomain implements DomainGenerator {
+	
+	public static Domain								DOMAIN = null;	
+	
+	public Domain generateDomain(){
+		
+		return DOMAIN;
+		
+	}
+
+}
