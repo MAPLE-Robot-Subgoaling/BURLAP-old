7bbe91a4cc62402bcc305d237dd6b48b778da9c4
Bhuvana Bellala <bbella1@umbc.edu>
2013-07-03 13:00:34 -0400

    For now, added all the constants

--- OOMDP2.0/src/edu/umbc/cs/maple/oomdp/rodexperiment/RodExperimentDomain.java
+++ OOMDP2.0/src/edu/umbc/cs/maple/oomdp/rodexperiment/RodExperimentDomain.java
@@ -1,13 +1,36 @@
 package edu.umbc.cs.maple.oomdp.rodexperiment;
 
-
-
 import edu.umbc.cs.maple.domain.oomdp.DomainGenerator;
 import edu.umbc.cs.maple.oomdp.Domain;
 
 public class RodExperimentDomain implements DomainGenerator {
 	
-	public static Domain								DOMAIN = null;	
+	public static final String				XATTNAME = "xAtt"; //x attribute
+	public static final String				YATTNAME = "yAtt"; //y attribute
+	
+	public static final String				AATTNAME = "angATT"; //Angle of the rod
+	
+	public static final String				LATTNAME = "lAtt"; //left boundary 
+	public static final String				RATTNAME = "rAtt"; //right boundary
+	public static final String				BATTNAME = "bAtt"; //bottom boundary
+	public static final String				TATTNAME = "tAtt"; //top boundary
+	
+	//all the objects in the domain
+	public static final String				AGENTCLASS = "agent";
+	public static final String				OBSTACLECLASS = "obstacle";
+	public static final String				GOALCLASS = "goal";
+	
+	//All the actions available to the agent
+	public static final String				ACTIONMOVEUP = "moveUp"; //moves one unit up
+	public static final String				ACTIONMOVEDOWN = "moveDown"; //moves one unit down
+	public static final String				ACTIONUPTHRUST = "upThrust"; //rotates 10 degrees
+	public static final String				ACTIONDOWNTHRUST = "downThrust"; //rotates -10 degrees
+	
+	//Propositional Fuctions
+	public static final String				PFTOUCHGOAL = "reachedGoal";
+	public static final String				PFTOUCHSURFACE = "touchingSurface"; //touching an obstacle
+	
+	public static Domain					DOMAIN = null;	
 	
 	public Domain generateDomain(){
 		
