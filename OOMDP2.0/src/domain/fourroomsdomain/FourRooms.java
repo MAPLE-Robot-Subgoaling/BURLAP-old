a91a114c61d9302325595ec6fe32cd1b91df4e55
Tenji Tembo <us74092@umbc.edu>
2013-07-24 13:59:23 -0400

    edits
    changed explorer mode

--- OOMDP2.0/src/domain/fourroomsdomain/FourRooms.java
+++ OOMDP2.0/src/domain/fourroomsdomain/FourRooms.java
@@ -77,7 +77,7 @@ public class FourRooms implements DomainGenerator {
 		State s = FourRooms.getCleanState();
 		setAgent(s, 1, 1);
 		setGoal(s, 11, 11);
-		int expMode = 5;
+		int expMode = 2;
 		
 		if(expMode == 0){		//Terminal Explorer
 			TerminalExplorer exp = new TerminalExplorer(d);
