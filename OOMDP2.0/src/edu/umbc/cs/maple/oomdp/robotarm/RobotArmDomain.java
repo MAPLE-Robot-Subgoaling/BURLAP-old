283570ce21c34447fbe26ec4e6bb34f598535cbe
Bhuvana Bellala <bbella1@umbc.edu>
2013-08-21 01:51:11 -0400

    Finished the Arm Domain
    However, still have to run through the CMEC!

--- OOMDP2.0/src/edu/umbc/cs/maple/oomdp/robotarm/RobotArmDomain.java
+++ OOMDP2.0/src/edu/umbc/cs/maple/oomdp/robotarm/RobotArmDomain.java
@@ -1,12 +1,15 @@
 package edu.umbc.cs.maple.oomdp.robotarm;
 
+import java.util.List;
+
 import edu.umbc.cs.maple.oomdp.Action;
 import edu.umbc.cs.maple.oomdp.Attribute;
 import edu.umbc.cs.maple.oomdp.Domain;
 import edu.umbc.cs.maple.oomdp.ObjectClass;
+import edu.umbc.cs.maple.oomdp.ObjectInstance;
 import edu.umbc.cs.maple.oomdp.PropositionalFunction;
 import edu.umbc.cs.maple.oomdp.State;
-import edu.umbc.cs.maple.oomdp.rodexperiment.RodExperimentDomain;
+import edu.umbc.cs.maple.oomdp.explorer.TerminalExplorer;
 
 /**
  * Class details:
@@ -19,6 +22,7 @@ import edu.umbc.cs.maple.oomdp.rodexperiment.RodExperimentDomain;
  * @author Bhuvana Bellala
  *
  */
+
 public class RobotArmDomain {
 
 	//The agent is defined by three points
@@ -75,13 +79,89 @@ public class RobotArmDomain {
 	//Propositional functions
 	//PFTOUCHGOAL = when the hand(the fingers attached part) touches the goal
 	//			Doesn't count when the arm touches the goal
-	//PFTOUCHOBSTACLE = when the agnts touch the obstacle
+	//PFTOUCHOBSTACLE = when the agents touch the obstacle
 	//			Obstacle Avoidance
 	public static final String				PFTOUCHGOAL = "touchedGoal";
 	public static final String				PFTOUCHOBSTACLE = "touchedObstacle";
 
 	//Initialized an arm domain
 	public static Domain					ROBOTARMDOMAIN = null;
+
+	public static void main(String[] args){
+		RobotArmDomain rad = new RobotArmDomain();
+		rad.generateDomain();
+
+		State clean= rad.getCleanState();
+
+		rad.setAgent(clean, 0., 3., 0., 5., 8., 0, 10., 11.);
+		rad.setObstacle(clean, 20., 30., 40., 60.);
+		rad.setGoal(clean, 40., 42., 38., 34.);
+
+		TerminalExplorer te = new TerminalExplorer(ROBOTARMDOMAIN);
+
+		te.addActionShortHand("w", ACTIONMOVEUP);
+		te.addActionShortHand("s", ACTIONMOVEDOWN);
+		te.addActionShortHand("a", ACTIONTURNARMLEFT);
+		te.addActionShortHand("d", ACTIONTURNARMRIGHT);
+		te.addActionShortHand("e", ACTIONTURNHANDRIGHT);
+		te.addActionShortHand("q", ACTIONTURNHANDLEFT);
+
+		te.exploreFromState(clean);
+
+	}
+
+	public State getCleanState(){
+
+		this .generateDomain();
+
+		State s = new State();
+
+		ObjectInstance agent = new ObjectInstance(ROBOTARMDOMAIN.getObjectClass(AGENTCLASS), AGENTCLASS + "0");
+		s.addObject(agent);
+
+		ObjectInstance goal = new ObjectInstance(ROBOTARMDOMAIN.getObjectClass(GOALCLASS), GOALCLASS + "0");
+		s.addObject(goal);
+
+
+		ObjectInstance obst = new ObjectInstance(ROBOTARMDOMAIN.getObjectClass(OBSTACLECLASS), OBSTACLECLASS + "0");
+		s.addObject(obst);
+
+
+		return s;
+
+	}
+
+	public void setAgent(State s, double x, double y, double a1, double x1, double y1, double a2, double x2, double y2){
+
+		ObjectInstance agent = s.getObjectsOfTrueClass(AGENTCLASS).get(0);
+
+		agent.setValue(A1ATTNAME, a1);
+		agent.setValue(XATTNAME, x);
+		agent.setValue(YATTNAME, y);
+		agent.setValue(X1ATTNAME, x1);
+		agent.setValue(Y1ATTNAME, y1);
+		agent.setValue(A2ATTNAME, a2);
+		agent.setValue(X2ATTNAME, x1);
+		agent.setValue(Y2ATTNAME, y1);
+	}
+
+	public void setObstacle(State s, double l, double r, double b, double t){
+		ObjectInstance obst = s.getObjectsOfTrueClass(OBSTACLECLASS).get(0);
+
+		obst.setValue(LATTNAME, l);
+		obst.setValue(RATTNAME, r);
+		obst.setValue(BATTNAME, b);
+		obst.setValue(TATTNAME, t);
+	}
+
+	public void setGoal(State s, double l, double r, double b, double t){
+		ObjectInstance goal = s.getObjectsOfTrueClass(GOALCLASS).get(0);
+
+		goal.setValue(LATTNAME, l);
+		goal.setValue(RATTNAME, r);
+		goal.setValue(BATTNAME, b);
+		goal.setValue(TATTNAME, t);
+	}
 
 	/**This functions generates the attributes for all the objects in the domain
 	 *Adds the objects, actions, and proposition functions to the domain
@@ -166,9 +246,141 @@ public class RobotArmDomain {
 
 		//add propositional functions to the domain
 		PropositionalFunction touchGoal = new TouchGoalPF(PFTOUCHGOAL, ROBOTARMDOMAIN, new String[]{AGENTCLASS, GOALCLASS});
-		PropositionalFunction touchObstacle = new TouchObstacleePF(PFTOUCHOBSTACLE, ROBOTARMDOMAIN, new String[]{AGENTCLASS, OBSTACLECLASS});
+		PropositionalFunction touchObstacle = new TouchObstaclePF(PFTOUCHOBSTACLE, ROBOTARMDOMAIN, new String[]{AGENTCLASS, OBSTACLECLASS});
 
 		return ROBOTARMDOMAIN;
+	}
+
+	/**
+	 * Updates the agents position based on the action chosen
+	 * @param st: the current state of the agent
+	 * @param moveUpDown: determines whether the whole hand should move up or down
+	 * 			+1.0 - the whole hand moves up; -1.0 - moved down 1; 0 - do not move the whole hand up or down 1
+	 * @param turnArmLeftRight: determines whether the arm should turn right or left
+	 * @param turnHandLeftRight: determines whether the hand should turn right or left at the wrist
+	 */
+	public static void updateMotion(State st, double moveUpDown, double turnArmLeftRight, double turnHandLeftRight){
+
+		ObjectInstance agent = st.getObjectsOfTrueClass(AGENTCLASS).get(0);
+
+		//All the attributes of the agent
+		double ang1 = agent.getRealValForAttribute(A1ATTNAME);
+		double ang2 = agent.getRealValForAttribute(A2ATTNAME);
+		double x = agent.getRealValForAttribute(XATTNAME);
+		double y = agent.getRealValForAttribute(YATTNAME);
+		double x1 = agent.getRealValForAttribute(X1ATTNAME);
+		double y1 = agent.getRealValForAttribute(Y1ATTNAME);
+		double x2 = agent.getRealValForAttribute(X2ATTNAME);
+		double y2 = agent.getRealValForAttribute(Y2ATTNAME);
+
+		//get temporary values, so if any of the values do not work we can revert back
+		double tempAng1 = agent.getRealValForAttribute(A1ATTNAME);
+		double tempAng2 = agent.getRealValForAttribute(A2ATTNAME);
+		double tempX = agent.getRealValForAttribute(XATTNAME);
+		double tempY = agent.getRealValForAttribute(YATTNAME);
+		double tempX1 = agent.getRealValForAttribute(X1ATTNAME);
+		double tempY1 = agent.getRealValForAttribute(Y1ATTNAME);
+		double tempX2 = agent.getRealValForAttribute(X2ATTNAME);
+		double tempY2 = agent.getRealValForAttribute(Y2ATTNAME);
+
+
+		//First, if the actions calls the whole hand to be moved up or down
+		if(moveUpDown == 1.0 || moveUpDown == -1.0){		
+			x = x + moveUpDown;
+			y = y + moveUpDown;
+			x1 = x1 + moveUpDown;
+			y1 = y1 + moveUpDown;
+			x2 = x2 + moveUpDown;
+			y2 = y2 + moveUpDown;
+		}
+
+		//If the arm is intended to turn left or right
+		if(turnArmLeftRight == 1.0 || turnArmLeftRight == -1.0){
+
+			//First change the angle of the arm
+			ang1 = ang1 + (turnArmLeftRight * ANGLEINC);
+			//Then rotate the (x1,y1) point around the (x,y)
+			//Then change the (x2, y2) points accordingly
+			double cosTheta = Math.cos(ANGLEINC * turnArmLeftRight);
+			double sinTheta = Math.cos(ANGLEINC * turnArmLeftRight);
+			x1 = ((cosTheta * (x1 - x)) - (sinTheta * (y1 - y)) + x);
+			y1 = ((cosTheta * (y1 - y)) + (sinTheta * (x1 - x)) + y);
+
+			//Find the difference (x1, y1) was changed
+			//Then use the difference to change the (x2, y2) positions
+			double changeX = x1 - tempX1;
+			double changeY = y1 - tempY1;
+			x2 = changeX + x2;
+			y2 = changeY + y2;
+
+		}
+
+		//If the hand is intended to tuwn left or right
+		if(turnHandLeftRight == 1.0 || turnHandLeftRight == -1.0){
+
+			//first change the angle of the wrist
+			ang2 = ang2 + (turnHandLeftRight * ANGLEINC);
+
+			//Then rotate the point (x2, y2) around (x1, y1)
+			double cosTheta = Math.cos(ANGLEINC * turnHandLeftRight);
+			double sinTheta = Math.cos(ANGLEINC * turnHandLeftRight);
+			x1 = ((cosTheta * (x2 - x1)) - (sinTheta * (y2 - y1)) + x1);
+			y1 = ((cosTheta * (y2 - y1)) + (sinTheta * (x2 - x1)) + y1);
+
+		}
+
+		//Now check for out of bounds
+		if(x < XMIN || x > XMAX || x1 < XMIN || x1 > XMAX || x2 < XMIN || x2 > XMAX || 
+				y < YMIN || y > YMAX ||y1 < YMIN || y1 > YMAX || y2 < YMIN || y2 > YMAX ||
+				ang1 > ANGLEMAX || ang1 < -ANGLEMAX ||
+				ang2 > (ang1 + ANGLEMAX) ||  ang2 < (-ang1 - ANGLEMAX)){
+
+			ang1 = tempAng1;
+			ang2 = tempAng2;
+			x = tempX;
+			y = tempY;
+			x1 = tempX1;
+			y1 = tempY1;
+			x2 = tempX2;
+			y2 = tempY2;
+
+		}
+
+		//Also check for object collisions
+		List <ObjectInstance> obstacles = st.getObjectsOfTrueClass(OBSTACLECLASS);
+		for(ObjectInstance o : obstacles){
+			double l = o.getRealValForAttribute(LATTNAME);
+			double r = o.getRealValForAttribute(RATTNAME);
+			double b = o.getRealValForAttribute(BATTNAME);
+			double t = o.getRealValForAttribute(TATTNAME);
+
+			if((x >= l && x <= r && y >= b && y <= t) || 
+					(x1 >= l && x1 <= r && y1 >= b && y1 <= t) ||
+					(x2 >= l && x2 <= r && y2 >= b && y2 <= t)){
+
+				ang1 = tempAng1;
+				ang2 = tempAng2;
+				x = tempX;
+				y = tempY;
+				x1 = tempX1;
+				y1 = tempY1;
+				x2 = tempX2;
+				y2 = tempY2;
+
+				//can only hit one obsbtacle so break out of search
+				break;
+			}
+		}
+
+		agent.setValue(XATTNAME, x);
+		agent.setValue(YATTNAME, y);
+		agent.setValue(X1ATTNAME, x1);
+		agent.setValue(Y1ATTNAME, y1);
+		agent.setValue(A1ATTNAME, ang1);
+		agent.setValue(X2ATTNAME, x2);
+		agent.setValue(Y2ATTNAME, y2);
+		agent.setValue(A2ATTNAME, ang2);
+
 	}
 
 	/**Defines the ActionMoveUp for the current domain
@@ -193,7 +405,7 @@ public class RobotArmDomain {
 			return st;
 		}
 	}
-	
+
 	/**
 	 * This action moves the whole hand down 1
 	 */
@@ -253,7 +465,7 @@ public class RobotArmDomain {
 			return st;
 		}
 	}
-	
+
 	/**
 	 * This action rotates the hand pi/18 R to the right
 	 */
@@ -293,4 +505,86 @@ public class RobotArmDomain {
 			return st;
 		}
 	}
+
+	/**Propositional function to check whether the hand has touched the goal
+	 * If the arm touches the goal, the function returns false
+	 */
+	public class TouchGoalPF extends PropositionalFunction{
+
+		public TouchGoalPF(String name, Domain domain, String parameterClasses) {
+			super(name, domain, parameterClasses);
+		}
+
+		public TouchGoalPF(String name, Domain domain, String[] parameterClasses) {
+			super(name, domain, parameterClasses);
+		}
+
+		@Override
+		public boolean isTrue(State st, String[] params) {
+			ObjectInstance agent = st.getObject(params[0]);
+			ObjectInstance goal = st.getObject(params[1]);
+
+			double l = goal.getRealValForAttribute(LATTNAME);
+			double r = goal.getRealValForAttribute(RATTNAME);
+			double b = goal.getRealValForAttribute(BATTNAME);
+			double t = goal.getRealValForAttribute(TATTNAME);
+
+			double x1 = agent.getRealValForAttribute(X1ATTNAME);
+			double y1 = agent.getRealValForAttribute(Y1ATTNAME);
+			double x2 = agent.getRealValForAttribute(X2ATTNAME);
+			double y2 = agent.getRealValForAttribute(Y2ATTNAME);
+
+			//If the (x1,y1) or (x2,y2); which basically represent the dimensions of the had are within the
+			//dinemsions of the box, the propositional function will return true
+			if((x1 >=l && x1 <= r && y1 <=t && y1>=b) || (x2 >=l && x2 <= r && y2 <=t && y2 >= b)){
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	/**Used for obstacle avoidance
+	 * If any of the dimensions of the agents hand are within the dimesions of the obstacles return true
+	 */
+	public class TouchObstaclePF extends PropositionalFunction{
+
+		public TouchObstaclePF(String name, Domain domain, String parameterClasses) {
+			super(name, domain, parameterClasses);
+		}
+
+		public TouchObstaclePF(String name, Domain domain, String [] parameterClasses) {
+			super(name, domain, parameterClasses);
+		}
+
+		@Override
+		public boolean isTrue(State st, String[] params) {
+
+
+			ObjectInstance agent = st.getObject(params[0]);
+			ObjectInstance o = st.getObject(params[1]);
+			double x = agent.getRealValForAttribute(XATTNAME);
+			double y = agent.getRealValForAttribute(YATTNAME);
+			double x1 = agent.getRealValForAttribute(XATTNAME);
+			double y1 = agent.getRealValForAttribute(YATTNAME);
+			double x2 = agent.getRealValForAttribute(XATTNAME);
+			double y2 = agent.getRealValForAttribute(YATTNAME);
+
+			double l = o.getRealValForAttribute(LATTNAME);
+			double r = o.getRealValForAttribute(RATTNAME);
+			double b = o.getRealValForAttribute(BATTNAME);
+			double t = o.getRealValForAttribute(TATTNAME);
+
+			if((x >= l && x <= r && y >= b && y <= t) || (x1 >= l && x1 <= r && y1 >= b && y1 <= t) || (x2 >= l && x2 <= r && y2 >= b && y2 <= t)){
+				return true;
+			}
+
+			return false;
+		}
+
+
+
+	}
+
+
 }
