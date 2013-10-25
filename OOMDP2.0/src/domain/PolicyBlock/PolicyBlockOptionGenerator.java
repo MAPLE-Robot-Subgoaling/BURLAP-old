44b4ac79d0c5c1f51b9efe1f7b5309726fad9df9
Tenji Tembo <us74092@umbc.edu>
2013-10-25 12:14:30 -0400

    Create the Blank Policy Block Option Generator
    This will generate options used from the Policy Blocks.

---
+++ OOMDP2.0/src/domain/PolicyBlock/PolicyBlockOptionGenerator.java
@@ -0,0 +1,8 @@
+package domain.PolicyBlock;
+
+//This class is designed to read in the options generated from the policy blocks and created options based on the 
+//merge, score, and subtract method described in the Pickett & Barto Paper: PolicyBlocks
+
+public class PolicyBlockOptionGenerator {
+
+}
