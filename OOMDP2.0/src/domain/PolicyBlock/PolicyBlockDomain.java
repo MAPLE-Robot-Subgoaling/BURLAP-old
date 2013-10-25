c1533ef8cd1eae970d91f686653385005f5a5a6c
Tenji Tembo <us74092@umbc.edu>
2013-10-25 12:12:25 -0400

    Policy Block Domain Created
    Created the Domain for Policy Blocks. The domain will generate the
    Q-Learning Episodes that will be used to generate the Policy Block
    Options explained in the Pickett & Barto Paper.

--- OOMDP2.0/src/domain/PolicyBlock/PolicyBlockDomain.java
+++ OOMDP2.0/src/domain/PolicyBlock/PolicyBlockDomain.java
@@ -5,15 +5,23 @@ import burlap.oomdp.auxiliary.StateParser;
 import burlap.oomdp.core.Domain;
 import burlap.oomdp.core.ObjectInstance;
 import burlap.oomdp.singleagent.RewardFunction;
+import burlap.oomdp.singleagent.common.SinglePFTF;
+import burlap.oomdp.singleagent.common.UniformCostRF;
+import burlap.oomdp.visualizer.Visualizer;
 import burlap.oomdp.core.State;
 import burlap.oomdp.core.TerminalFunction;
+import burlap.behavior.singleagent.EpisodeAnalysis;
+import burlap.behavior.singleagent.EpisodeSequenceVisualizer;
+import burlap.behavior.singleagent.learning.LearningAgent;
+import burlap.behavior.singleagent.learning.tdmethods.QLearning;
 import burlap.behavior.singleagent.planning.StateConditionTest;
+import burlap.behavior.singleagent.planning.deterministic.TFGoalCondition;
 import burlap.behavior.statehashing.DiscreteStateHashFactory;
 import burlap.oomdp.core.*;
 
 public class PolicyBlockDomain {
 
-	GridWorldDomain policyDomain;
+	GridWorldDomain policyBlock;
 	Domain domain;
 	StateParser sp;
 	RewardFunction rf;
@@ -22,9 +30,55 @@ public class PolicyBlockDomain {
 	State initialState;
 	DiscreteStateHashFactory hashFactory;
 	
-	
+	//Main Function
 	public static void main(String[] args) {
+		PolicyBlockDomain blocks = new PolicyBlockDomain();
+		String output = "policyBlocks";
+		blocks.QLearn(output);
+		blocks.visualize(output);
+	}
+	
+	public PolicyBlockDomain(){
+		//create the gridworld
+		policyBlock = new GridWorldDomain(11,11);
+		policyBlock.setMapToFourRooms();
+		domain = policyBlock.generateDomain();
 		
+		//define the parser, reward, and termination conditions
+		sp = new GridWorldStateParser(domain);
+		rf = new UniformCostRF();
+		tf = new SinglePFTF(domain.getPropFunction(GridWorldDomain.PFATLOCATION));
+		goalCondition = new TFGoalCondition(tf);
+		
+		//set up initial state
+		initialState = GridWorldDomain.getOneAgentOneLocationState(domain);
+		GridWorldDomain.setAgent(initialState, 0, 0);
+		GridWorldDomain.setLocation(initialState, 0, 10, 10);
+		
+		//set up the state hashing system
+		hashFactory = new DiscreteStateHashFactory();
+		hashFactory.setAttributesForClass(GridWorldDomain.CLASSAGENT, domain.getObjectClass(GridWorldDomain.CLASSAGENT).attributeList); //uses agent position to hash
+	}
+	
+	//create the visualizer for the policyBlock
+	public void visualize(String output){
+		Visualizer v = GridWorldVisualizer.getVisualizer(domain, policyBlock.getMap());
+		EpisodeSequenceVisualizer evis = new EpisodeSequenceVisualizer(v, domain, sp, output);
+	}
+	
+	//Learning Algorithm
+	public void QLearn(String output){
+		if(!output.endsWith("/")){
+			output = output + "/";
+		}
+		
+		LearningAgent agent = new QLearning(domain, rf, tf, 0.99, hashFactory, 0., 0.9); //create the QLearning agent
+		
+		for(int i = 0; i < 100; i++){
+			EpisodeAnalysis ea = agent.runLearningEpisodeFrom(initialState); //run the episode
+			ea.writeToFile(String.format("%se%03d", output, i), sp); //record the episode
+			System.out.println("Episode "+ i + " : " + ea.numTimeSteps()); //print the performance of the episode
+		}
 	}
 
 }
