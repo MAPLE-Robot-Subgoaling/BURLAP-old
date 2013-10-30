a1f013f78f1c4c6c850648cf84157eeebed55a8d
Tenji Tembo <us74092@umbc.edu>
2013-10-30 12:20:40 -0400

    commenting
    added some comments to PolicyBlockOptionGenerator

--- OOMDP2.0/src/domain/PolicyBlock/PolicyBlockOptionGenerator.java
+++ OOMDP2.0/src/domain/PolicyBlock/PolicyBlockOptionGenerator.java
@@ -7,21 +7,22 @@ public class PolicyBlockOptionGenerator {
 
 	PolicyBlockDomain environ;
 	
+	//Main
 	public static void main(String args[]){
 		PolicyBlockOptionGenerator generator = new PolicyBlockOptionGenerator();
 		generator.generatePolicies();
 	}
 	
+	//creates a new Policy Domain Object
 	public PolicyBlockOptionGenerator(){
 		environ = new PolicyBlockDomain();
 	}
 	
-	public void generatePolicies(){
-		
+	//Generates 5 iterations which contains 100 policies run via Q-Learning
+	public void generatePolicies(){	
 		for(int i = 0; i < 5; i++){
 			environ.QLearn("policyBlocks/" +  i + "-set/");
 		}
-		
 		environ.visualize("policyBlocks/2-set/");
 	}
 }
