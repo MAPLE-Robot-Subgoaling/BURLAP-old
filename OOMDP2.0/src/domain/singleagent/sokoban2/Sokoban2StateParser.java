1e59f13006e1a6f432f5fa4061f979e947986901
Tenji Tembo <us74092@umbc.edu>
2014-01-09 10:20:24 -0500

    Sokoban State Parser
    Creating a State Parser for Sokoban. Borrowing the Same format used in
    Four Rooms State Parser

---
+++ OOMDP2.0/src/domain/singleagent/sokoban2/Sokoban2StateParser.java
@@ -0,0 +1,21 @@
+package domain.singleagent.sokoban2;
+
+import burlap.oomdp.auxiliary.StateParser;
+import burlap.oomdp.core.ObjectInstance;
+import burlap.oomdp.core.State;
+
+public class Sokoban2StateParser implements StateParser{
+	
+	@Override
+	public String stateToString(State s){
+		
+		return "";
+	}
+	
+	@Override
+	public State stringToState(String str){
+		
+		return null;
+	}
+
+}
