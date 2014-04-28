680312957cf8d475dc89213c9136accd04a65815
Tenji Tembo <us74092@umbc.edu>
2014-04-28 10:19:47 -0400

    Prepping for Push to Master

--- OOMDP2.0/src/domain/PolicyBlock/FourRoomsOptions.java
+++ OOMDP2.0/src/domain/PolicyBlock/FourRoomsOptions.java
@@ -17,23 +17,23 @@ public class FourRoomsOptions {
 		String original_output = "Original/";
 		String options_output = "Options/";
 		PolicyBlockDomain domain = new PolicyBlockDomain();
-		TrajectoryGenerator trajectory = new TrajectoryGenerator(trajectory_output);
+		//TrajectoryGenerator trajectory = new TrajectoryGenerator(trajectory_output);
 		
 		//running the original domain via Q-Learning
 		System.out.println("Running Q-learning");
-		domain.QLearn(original_output);
+		//domain.QLearn(original_output);
 		domain.visualize(original_output);
 		
 		//running trajectory generator with 3 generated policies
-		System.out.println("Creating Trajectory Options...");
+		/*System.out.println("Creating Trajectory Options...");
 		trajectory.runSim(3);
 		ArrayList<Option> options = trajectory.createOptions();
 		System.out.println("Number of Options: " + options.size());
-		domain.addOptions(options);
+		domain.addOptions(options);*/
 		
 		//Running with Options
-		System.out.println("\nRunning with Options");
-		domain.QLearn(options_output);
+		//System.out.println("\nRunning with Options");
+		//domain.QLearn(options_output);
 		domain.visualize(options_output);
 		
 	}
