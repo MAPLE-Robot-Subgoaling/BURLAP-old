c1467f330e72ea049c03d2d6cef442de209702fd
hanicho <hanicho1@umbc.edu>
2014-12-22 12:03:42 -0500

    bug fix

--- src/burlap/behavior/policyblocks/AbstractedPolicy.java
+++ src/burlap/behavior/policyblocks/AbstractedPolicy.java
@@ -910,6 +910,13 @@ public class AbstractedPolicy extends Policy {
 		objSet.addAll(m1.keySet());
 
 		for (String obj : objSet) {
+		    if (m0.get(obj) == null) {
+		    	m0.put(obj, 0);
+		    }
+		    if (m1.get(obj) == null) {
+		    	m1.put(obj, 0);
+		    }
+		    
 		    if (m0.get(obj) < 1 && m1.get(obj) < 1) {
 		    } else if (m0.get(obj) > m1.get(obj)) {
 			m0.put(obj, m0.get(obj) - 1);
