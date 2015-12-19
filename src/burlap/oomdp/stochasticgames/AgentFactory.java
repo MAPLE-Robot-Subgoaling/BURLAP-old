9768106696438c30d5da83f88a6ee9e649759f10
Nicholas Haltmeyer <hanicho1@umbc.edu>
2015-12-18 22:34:06 -0500

    wef

---
+++ src/burlap/oomdp/stochasticgames/AgentFactory.java
@@ -0,0 +1,16 @@
+package burlap.oomdp.stochasticgames;
+
+/**
+ * An interface for generating agents
+ * 
+ * @author James MacGlashan
+ * 
+ */
+public interface AgentFactory {
+	/**
+	 * Returns a new agent instance.
+	 * 
+	 * @return a new agent instance.
+	 */
+	public SGAgent generateAgent();
+}
